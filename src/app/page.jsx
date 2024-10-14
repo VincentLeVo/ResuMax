dca3d91b1d9a6b5291ea6f9fe754e4459c1e30cc
“VincentLeVo” <vincentlevo@gmail.com>
2024-10-13 22:44:31 -0700

    Finished Front-end Components - for now

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,21 +5,23 @@ import clsx from "clsx";
 import { Badge } from "@/components/Badge";
 import { KeyTerm } from "@/components/KeyTerm";
 import { Heading, Subheading } from "@/components/Text";
+import { PencilIcon, PlusIcon, XMarkIcon } from "@heroicons/react/24/solid";
+import { Divider } from "@/components/Divider";
+import { Textarea } from "@/components/Textarea";
 
 function Summary() {
   return (
-    <div className="flex justify-between items-center py-4 border-b border-gray-200">
-      <div className="flex flex-col gap-7">
+    <div className="flex justify-between items-center ">
+      <div className="flex flex-col gap-4">
         <div className="border-t border-white/10">
-          <div className="text-md/6 mt-6 mb-2  font-semibold">
-            Resume Status:
-          </div>
+          <div className="text-lg/6 mb-2  font-semibold">Resume Status:</div>
           <Badge color="green" size="large">
             Optimized
           </Badge>
         </div>
-        <div className="flex border-t border-white/10 flex-col mb-5 ">
-          <div className="text-md mt-6 mb-2 font-semibold">Match Score:</div>
+        <Divider />
+        <div className="flex flex-col">
+          <div className="text-lg mb-2 font-semibold">Match Score:</div>
           <div className="w-24 h-24 rounded-full bg-green-500/10 dark:text-green-400 flex items-center justify-center text-2xl font-bold">
             85%
           </div>
@@ -44,8 +46,8 @@ function KeyMetrics() {
   return (
     <div>
       <Subheading>Keyword Match</Subheading>
-      <div className=" rounded-lg p-4">
-        <table className="table-auto mt-3 whitespace-nowrap text-left w-full min-w-full text-sm/6 text-white">
+      <div className="mt-3 rounded-lg p-2">
+        <table className="table-auto whitespace-nowrap text-left w-full min-w-full text-sm/6 text-white">
           <thead className=" text-zinc-400">
             <tr className="text-left">
               <th className="border-b border-b-zinc-950/10 px-3 py-4 font-medium first:pl-[var(--gutter,theme(spacing.2))] last:pr-[var(--gutter,theme(spacing.2))] dark:border-b-white/10">
@@ -104,7 +106,7 @@ function ResumeBreakdown() {
   return (
     <div>
       <Subheading>Resume Breakdown</Subheading>
-      <dl className="mt-8 grid grid-cols-1 divide-y divide-white/10 overflow-hidden rounded-lg  shadow ">
+      <dl className="mt-3 grid grid-cols-1 divide-y divide-white/10 overflow-hidden rounded-lg  shadow ">
         <div className="px-4 border-t border-white/10 py-5 sm:p-6">
           <dt className="text-md font-semibold text-white  mb-2">
             Skills: 80%
@@ -143,182 +145,227 @@ function ResumeBreakdown() {
   );
 }
 
-function Analysis() {
+function Suggestions() {
+  let suggestions = [
+    {
+      title: "Add more technical keywords to match the job",
+      priority: "High",
+      type: "Add",
+    },
+    {
+      title: "Consider expanding leadership roles",
+      priority: "Medium",
+      type: "Add",
+    },
+    {
+      title: "Edit the work experience section about company culture",
+      priority: "Low",
+      type: "Edit",
+    },
+    {
+      title: "Delete the objective section",
+      priority: "High",
+      type: "Delete",
+    },
+  ];
+
+  const priorityStyles = {
+    High: "red",
+    Medium: "orange",
+    Low: "zinc",
+  };
+
+  const iconMapping = {
+    Add: PlusIcon,
+    Delete: XMarkIcon,
+    Edit: PencilIcon,
+  };
+
   return (
-    <div>
-      <div className="bg-gray-100/20">
-        <Subheading>Personalized Suggestions</Subheading>
-        <div className="mt-8">
-          <div className="flex flex-col gap-2">
-            <div className="flex items-center space-x-2 bg-white/10 p-4 rounded-lg shadow-md">
-              <span className="text-gray-600">
-                <svg
-                  className="w-6 h-6"
-                  fill="none"
-                  stroke="currentColor"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M12 4v16m8-8H4"
-                  />
-                </svg>
-              </span>
-              <div>
-                <p>Add more technical keywords to match the job</p>
-                <span className="text-sm text-yellow-600">High Priority</span>
-              </div>
-            </div>
-
-            <div className="flex items-center space-x-2 bg-white/10 p-4 rounded-lg shadow-md">
-              <span className="text-gray-600">
-                <svg
-                  className="w-6 h-6"
-                  fill="none"
-                  stroke="currentColor"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M12 4v16m8-8H4"
-                  />
-                </svg>
-              </span>
-              <div>
-                <p>Consider expanding leadership roles</p>
-                <span className="text-sm text-yellow-600">Medium Priority</span>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      {/* Resume Strengths */}
-      <div className="mt-6">
-        <h2 className="text-lg font-semibold mb-4">Resume Strengths</h2>
-        <div className="bg-gray-100 rounded-lg p-4">
-          <p className="text-green-600">
-            ✔ Your resume has strong industry-specific keywords. Continue
-            focusing on project experience.
-          </p>
-        </div>
+    <div className="mt-14">
+      <Subheading>Personalized Suggestions</Subheading>
+      <div className="mt-3">
+        <ul className="flex flex-col divide-y divide-white/10">
+          {suggestions.map((suggestion, index) => {
+            const Icon = iconMapping[suggestion.type]; // Dynamically selecting the icon
+            return (
+              <li key={index} className="w-full py-5">
+                <div className="flex content-center">
+                  <Icon className="text-gray-400 self-baseline w-6 h-6 mr-4" />
+                  <div>
+                    <p className="text-base font-semibold">
+                      {suggestion.title}
+                    </p>
+                    <Badge
+                      className="mt-1"
+                      color={priorityStyles[suggestion.priority]}
+                    >
+                      {suggestion.priority} Priority
+                    </Badge>
+                  </div>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
       </div>
     </div>
+  );
+}
+
+function Strengths() {
+  let strengths = [
+    {
+      title:
+        "Your resume has strong industry-specific keywords. It is well optimized.",
+    },
+    {
+      title: "Your education section is well-detailed.",
+    },
+    {
+      title: "Your skills in frontend are excellent.",
+    },
+    {
+      title: "Your resume has strong industry-specific keywords.",
+    },
+  ];
+
+  return (
+    /* Resume Strengths */
+    <div className="mt-14">
+      <Subheading>Strengths</Subheading>
+      <ul className="mt-3 divide-y divide-white/10">
+        {strengths.map((strength, index) => {
+          return (
+            <li key={index} className="w-full py-3">
+              <div className="flex items-center">
+                <div className="w-6 h-6 bg-zinc-500/10 rounded-full flex items-center justify-center text-zinc-500">
+                  ✔
+                </div>
+                <div className="ml-4">
+                  <p className="text-sm ">{strength.title}</p>
+                </div>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
+
+function UploadResume() {
+  return (
+    <Container>
+      <div className="md:flex md:items-center md:justify-between">
+        <div className="min-w-0 flex-1">
+          <h2 className="text-2xl font-bold leading-7 text-white sm:truncate sm:text-3xl sm:tracking-tight">
+            Please Upload Your Resume
+          </h2>
+        </div>
+      </div>
+      <div className="mt-2 max-w-md mx-auto flex  flex-col justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
+        <div className="text-center self-center">
+          <PaperClipIcon
+            aria-hidden="true"
+            className="mx-auto h-12 w-12 text-blue-400/50"
+          />
+          <div className="mt-4 flex items-center text-sm leading-6 text-slate-300">
+            <label
+              htmlFor="file-upload"
+              className="relative cursor-pointer rounded-md px-3 py-1 bg-blue-300/20 font-semibold text-slate-200 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-blue-500"
+            >
+              Upload a file
+              <input
+                id="file-upload"
+                name="file-upload"
+                type="file"
+                className="sr-only"
+              />
+            </label>
+            <p className="pl-1">or drag and drop</p>
+          </div>
+          <p className="text-xs mt-2 leading-5 text-slate-400">
+            PDF File Up To 10MB
+          </p>
+        </div>
+        <ul
+          role="list"
+          className="divide-y mt-3 divide-white/10 rounded-md border border-white/20"
+        >
+          <li className="flex items-center justify-between py-4 pl-4 pr-5 text-sm leading-6">
+            <div className="flex w-0 flex-1 items-center">
+              <PaperClipIcon
+                aria-hidden="true"
+                className="h-5 w-5 flex-shrink-0 text-gray-400"
+              />
+              <div className="ml-4 flex min-w-0 flex-1 gap-2">
+                <span className="truncate text-gray-400 font-medium">
+                  resume_back_end_developer.pdf
+                </span>
+                <span className="flex-shrink-0 text-gray-400">2.4mb</span>
+              </div>
+            </div>
+            <div className="ml-4 flex-shrink-0">
+              <a
+                href="#"
+                className="font-medium text-red-400 hover:text-indigo-300"
+              >
+                Remove
+              </a>
+            </div>
+          </li>
+          <li className="flex items-center justify-between py-4 pl-4 pr-5 text-sm leading-6">
+            <div className="flex w-0 flex-1 items-center">
+              <PaperClipIcon
+                aria-hidden="true"
+                className="h-5 w-5 flex-shrink-0 text-gray-400"
+              />
+              <div className="ml-4 flex min-w-0 flex-1 gap-2">
+                <span className="truncate text-gray-400 font-medium">
+                  coverletter_back_end_developer.pdf
+                </span>
+                <span className="flex-shrink-0 text-gray-400">4.5mb</span>
+              </div>
+            </div>
+            <div className="ml-4 flex-shrink-0">
+              <a
+                href="#"
+                className="font-medium text-red-400 hover:text-indigo-300"
+              >
+                Remove
+              </a>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </Container>
+  );
+}
+
+function OptionalJobDescription() {
+  return (
+    <Container>
+      <div>
+        <label
+          htmlFor="comment"
+          className="block text-xs/6 font-medium leading-6 text-zinc-400"
+        >
+          Optional: Write in your Job Description.
+        </label>
+        <div className="mt-2">
+          <Textarea id="comment" name="comment" rows={4} defaultValue={""} />
+        </div>
+      </div>
+    </Container>
   );
 }
 
 export default function Home() {
   return (
     <>
-      <Container>
-        <div className="md:flex md:items-center md:justify-between">
-          <div className="min-w-0 flex-1">
-            <h2 className="text-2xl font-bold leading-7 text-white sm:truncate sm:text-3xl sm:tracking-tight">
-              Please Upload Your Resume
-            </h2>
-          </div>
-        </div>
-        <div className="mt-2 max-w-md mx-auto flex  flex-col justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
-          <div className="text-center self-center">
-            <PaperClipIcon
-              aria-hidden="true"
-              className="mx-auto h-12 w-12 text-blue-400/50"
-            />
-            <div className="mt-4 flex items-center text-sm leading-6 text-slate-300">
-              <label
-                htmlFor="file-upload"
-                className="relative cursor-pointer rounded-md px-3 py-1 bg-blue-300/20 font-semibold text-slate-200 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-blue-500"
-              >
-                Upload a file
-                <input
-                  id="file-upload"
-                  name="file-upload"
-                  type="file"
-                  className="sr-only"
-                />
-              </label>
-              <p className="pl-1">or drag and drop</p>
-            </div>
-            <p className="text-xs leading-5 text-slate-400">
-              PDF File Up To 10MB
-            </p>
-          </div>
-          <ul
-            role="list"
-            className="divide-y divide-white/10 rounded-md border border-white/20"
-          >
-            <li className="flex items-center justify-between py-4 pl-4 pr-5 text-sm leading-6">
-              <div className="flex w-0 flex-1 items-center">
-                <PaperClipIcon
-                  aria-hidden="true"
-                  className="h-5 w-5 flex-shrink-0 text-gray-400"
-                />
-                <div className="ml-4 flex min-w-0 flex-1 gap-2">
-                  <span className="truncate text-gray-400 font-medium">
-                    resume_back_end_developer.pdf
-                  </span>
-                  <span className="flex-shrink-0 text-gray-400">2.4mb</span>
-                </div>
-              </div>
-              <div className="ml-4 flex-shrink-0">
-                <a
-                  href="#"
-                  className="font-medium text-red-400 hover:text-indigo-300"
-                >
-                  Remove
-                </a>
-              </div>
-            </li>
-            <li className="flex items-center justify-between py-4 pl-4 pr-5 text-sm leading-6">
-              <div className="flex w-0 flex-1 items-center">
-                <PaperClipIcon
-                  aria-hidden="true"
-                  className="h-5 w-5 flex-shrink-0 text-gray-400"
-                />
-                <div className="ml-4 flex min-w-0 flex-1 gap-2">
-                  <span className="truncate text-gray-400 font-medium">
-                    coverletter_back_end_developer.pdf
-                  </span>
-                  <span className="flex-shrink-0 text-gray-400">4.5mb</span>
-                </div>
-              </div>
-              <div className="ml-4 flex-shrink-0">
-                <a
-                  href="#"
-                  className="font-medium text-red-400 hover:text-indigo-300"
-                >
-                  Remove
-                </a>
-              </div>
-            </li>
-          </ul>
-        </div>
-      </Container>
-      <Container>
-        <div>
-          <label
-            htmlFor="comment"
-            className="block text-sm font-medium leading-6 text-slate-200"
-          >
-            Optional: Write in your Job Description.
-          </label>
-          <div className="mt-2">
-            <textarea
-              id="comment"
-              name="comment"
-              rows={4}
-              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              defaultValue={""}
-            />
-          </div>
-        </div>
-      </Container>
+      <UploadResume />
+      <OptionalJobDescription />
       <Container>
         <Heading>Good afternoon, Erica</Heading>
         <div className="mt-8 flex items-end justify-between">
@@ -337,12 +384,13 @@ export default function Home() {
 
               <div className="px-4 py-6 sm:px-6 lg:pl-8 xl:flex-1 xl:pl-6">
                 <KeyMetrics />
+                <Suggestions />
+                <Strengths />
               </div>
             </div>
 
             <div className="shrink-0 border-t border-gray-200 px-4 py-6 sm:px-6 lg:w-96 lg:border-l lg:border-t-0 lg:pr-8 xl:pr-6">
               <ResumeBreakdown />
-              <Analysis />
             </div>
           </div>
         </div>
