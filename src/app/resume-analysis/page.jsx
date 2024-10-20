2c867feba2ada786b1bd2e32554e8b167fcd23a3
“VincentLeVo” <vincentlevo@gmail.com>
2024-10-20 01:07:34 -0700

    - Removed searchparams

--- src/app/resume-analysis/page.jsx
+++ src/app/resume-analysis/page.jsx
@@ -8,7 +8,6 @@ import { Strengths } from '@/components/Strengths'
 import { Suggestions } from '@/components/Suggestions'
 import { Summary } from '@/components/Summary'
 import { Heading } from '@/components/Text'
-import { useSearchParams } from 'next/navigation'
 import { useState, useEffect } from 'react'
 import clsx from 'clsx'
 export default function ResumeSuggestions() {
@@ -120,7 +119,6 @@ export default function ResumeSuggestions() {
 
   const [analysisData, setAnalysisData] = useState({})
   const [loading, setLoading] = useState(true)
-  const searchParams = useSearchParams()
 
   // Function to check if the data is fully loaded
   const isDataReady = (data) => {
@@ -170,7 +168,7 @@ export default function ResumeSuggestions() {
               : ','}
           </p>
           <Heading className="mt-1">Resume Analysis</Heading>
-          <div className="max-w-10xl mx-auto mt-14 w-full grow lg:flex">
+          <div className="mx-auto mt-14 w-full max-w-10xl grow lg:flex">
             {/* Left sidebar & main wrapper */}
             <div className="flex-1 xl:flex">
               <div className="shrink-1 flex flex-col gap-y-2 p-1 xl:min-w-[16rem] xl:max-w-[50rem]">
